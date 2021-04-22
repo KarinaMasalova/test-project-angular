a781d5e186a05c24184711bd8ac8cdb8d23f2f60
maksim.zalutski@leverx.com <Karyna.Masalava@LeverX.local>
2021-04-22 11:58:43 +0300

    lalalla

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,4 +1,9 @@
 import { NgModule } from '@angular/core';
+
+
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
